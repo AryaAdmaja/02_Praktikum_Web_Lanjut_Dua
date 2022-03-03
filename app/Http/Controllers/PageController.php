dceb3164f55edac29a8507bc57e8cf4fd5a95f75
AryaAdmaja <aryaadmaja19@gmail.com>
2022-03-03 21:56:37 +0800

    Kode Program 1 Praktikum 2

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+public function index(){
+    return "Selamat Datang";
+}
+public function about(){
+    return "Nama : Arya Admaja <br>NIM :2041720104";
+}
+public function articles($id){
+    return "Halaman Artikel Dengan ID $id";
+}
+
+}
